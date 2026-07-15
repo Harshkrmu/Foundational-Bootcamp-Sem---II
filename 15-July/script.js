aa3f26868bb5cf23ae81946c57b252aba3e93d53
Harshkrmu <harshescapematrix2008@gmail.com>
2026-07-15 15:09:12 +0530

    Yeah

---
+++ 15-July/script.js
@@ -0,0 +1,21 @@
+// console.log("Hello World!")
+
+// let username = "Harsh";
+// let Birth_Year = 1999;
+// console.log("username :",username)
+// console.log("Birth_Year :",Birth_Year)
+
+// let num = Math.floor(Math.random()*10)+1
+// console.log(num)
+
+// let a = true;
+// if (a){
+//     console.log("True")
+// }
+// else{
+//     console.log("False")
+// }
+
+// for (i=1;i<6;i++) {
+//     console.log(i)
+// }
